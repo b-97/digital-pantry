3fe2b84d5bf4cc1f54af11d7f0e72136cc51fe32
Jacob Ranson <jacobrileighranson@gmail.com>
2017-11-24 14:23:25 -0500

    Added outline of client & server js, also gitignore

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 // Digital Pantry Server Side JavaScript
+
+// Node Modules
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql  = require('mysql');
+
+// Custom Modules
+var page_manager = require('./controllers/page_manager');
+var table_manager = require('./controllers/table_manager');
+
+app.use(express.static("."));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.listen(8080, function() {
+	console.log("...Server Started...");
+});
