23ec15d4fe3a7ddaeaadcc5d9fab40e5d819c622
Michael Rinehart <mjr426@drexel.edu>
2017-11-28 20:53:26 -0500

    added documentation

--- server.js
+++ server.js
@@ -18,24 +18,30 @@ app.listen(8080, function() {
 	console.log("...Server Started...");
 });
 
-// Listening for requests
+
+/*
+	/homepage, /view_ingredients_page, /add_ingredients_page,
+	/view_recipes_page, /add_recipes_page are all POST requests
+	that simply load the HTML data for a particular page.
+*/
 app.get('/homepage', function(req, res) {
 	res.send(page_manager.renderHomePage());
 });
-// Listening for requests
 app.get('/view_ingredients_page', function(req, res) {
 	res.send(page_manager.renderViewIngredientsPage());
 });
-
-// Listening for requests
 app.get('/add_ingredients_page', function(req, res) {
 	res.send(page_manager.renderAddIngredientsPage());
 });
-// Listening for requests
 app.get('/view_recipes_page', function(req, res) {
 	res.send(page_manager.renderViewRecipesPage());
 });
-// Listening for requests
 app.get('/add_recipes_page', function(req, res) {
 	res.send(page_manager.renderCreateRecipePage());
+});
+
+//	get_ingredients_table - pulls the username and password
+// from the request, and calls 
+app.get('/get_ingredients_table', function(req, res){
+	
 });
